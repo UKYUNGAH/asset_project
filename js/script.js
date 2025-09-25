05e3012ee565821d6dac99d5ac88ffae16a38516
경아 <ukyunga0.1@gmail.com>
2025-09-26 02:59:44 +0900

    Feat: 애니메이션 진행중 40%

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ ScrollTrigger.create({
             y: 0,
             backgroundColor: 'var(--bs-white)',
             boxShadow: '0 2px 6px rgba(0,0,0,0.1)',
-            duration: 0.1, // 전환 속도 빠르게
-            ease: 'power1.out', // 가벼운 easing
+            duration: 0.1,
+            ease: 'power1.out',
         });
 
         gsap.to('.header li a:not(.on)', {
@@ -25,8 +25,33 @@ ScrollTrigger.create({
         });
 
         gsap.to('.header li a:not(.on)', {
-            color: 'var(--bs-white)', // 원래 색으로 확실히 복원
+            color: 'var(--bs-white)',
             duration: 0.1,
         });
     },
+});
+
+gsap.from('.business h3', {
+    scrollTrigger: {
+        trigger: '.business h3', // h3 자체 기준
+        start: 'top 90%', // h3의 top이 뷰포트 80% 지점에 왔을 때 시작
+        toggleActions: 'play none none reverse',
+    },
+    opacity: 0,
+    y: 60,
+    duration: 1,
+    ease: 'power2.out',
+});
+
+gsap.from('.business .item', {
+    scrollTrigger: {
+        trigger: '.business .card',
+        start: 'top 75%',
+        toggleActions: 'play none none reverse',
+    },
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    stagger: 0.2, // 카드 순서대로
+    ease: 'power3.out',
 });
